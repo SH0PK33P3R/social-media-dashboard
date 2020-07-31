6bddb82cad3895f2b481540c939f5e16291f2b2c
Dylan Carver <dylc2770@gmail.com>
2020-07-31 18:06:15 +0200

    remove unecessary pages/components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Nav from "modules/nav";
 import Home from "routes/home";
-import Crud from "routes/crud";
-import Layout from "routes/layout";
 
 function App() {
   return (
     <Router>
-      <Nav />
       <Switch>
-        <Route path="/crud">
-          <Crud />
-        </Route>
-        <Route path="/layout">
-          <Layout />
-        </Route>
         <Route path="/">
           <Home />
         </Route>
