b9cf5f9bc0d62fe00637c44295548fe2c47ac8a6
Dylan Carver <dylc2770@gmail.com>
2020-07-31 18:08:06 +0200

    add data file

---
+++ frontend/src/routes/home/data.js
@@ -0,0 +1,92 @@
+const data = {
+  main: [
+    {
+      platform: "facebook",
+      handle: "@nathanf",
+      number: 1907,
+      amountToday: 12,
+      positive: true,
+    },
+    {
+      platform: "twitter",
+      handle: "@nathanf",
+      number: 1044,
+      amountToday: 99,
+      positive: true,
+    },
+    {
+      platform: "instagram",
+      handle: "@realnathanf",
+      number: "11k",
+      amountToday: 1099,
+      positive: true,
+    },
+    {
+      platform: "youtube",
+      handle: "Nathan F.",
+      number: "8239",
+      amountToday: 144,
+      positive: false,
+    },
+  ],
+  today: [
+    {
+      type: "Page Views",
+      platform: "facebook",
+      amount: 87,
+      percent: 3,
+      positive: true,
+    },
+    {
+      type: "Likes",
+      platform: "facebook",
+      amount: 52,
+      percent: 2,
+      positive: false,
+    },
+    {
+      type: "Likes",
+      platform: "instagram",
+      amount: 5462,
+      percent: 2257,
+      positive: true,
+    },
+    {
+      type: "Profile Views",
+      platform: "instagram",
+      amount: "52k",
+      percent: 1375,
+      positive: true,
+    },
+    {
+      type: "Retweets",
+      platform: "twitter",
+      amount: 117,
+      percent: 303,
+      positive: true,
+    },
+    {
+      type: "Likes",
+      platform: "twitter",
+      amount: 507,
+      percent: 553,
+      positive: true,
+    },
+    {
+      type: "Likes",
+      platform: "youtube",
+      amount: 107,
+      percent: 19,
+      positive: false,
+    },
+    {
+      type: "Total Views",
+      platform: "youtube",
+      amount: 1507,
+      percent: 12,
+      positive: false,
+    },
+  ],
+};
+
+export default data;
